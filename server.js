753247dad0e6906fcb84602567085786d47b133d
Theron Blount <229767+straightjacket@users.noreply.github.com>
2025-09-06 20:38:53 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  res.set('Content-Security-Policy', "default-src 'self'");
+  next();
+});
+
+app.use(express.static('site/'));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Serving files from ${__dirname}`);
+});
